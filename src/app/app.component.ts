21b6ac1cc0642e5cc9188428318badbf4e202445
Daniel Olivieri <dfolivieri@gmail.com>
2022-01-03 17:59:01 -0500

    Bulma, new UI, and moment js
    Getting most of the MVP done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chronocrator';
+  hours = moment().format('h')
+  minutes = moment().format('mm')
+  milliseconds = moment().diff(moment().startOf('day'), 'milliseconds');
+  seconds = moment().diff(moment().startOf('day'), 'seconds');
+  dives = Math.trunc(this.milliseconds / 45000);
 }
+
+
