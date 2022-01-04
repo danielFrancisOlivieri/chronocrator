b9f502d75a1073e178a47932d7962c28f718eeb0
Daniel Olivieri <dfolivieri@gmail.com>
2022-01-04 11:19:10 -0500

    favicon, add metrics, update time
    angular -> clock
    Button to add new units of time
    Button to update time across app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
 import * as moment from 'moment';
 
 @Component({
@@ -8,11 +9,27 @@ import * as moment from 'moment';
 })
 export class AppComponent {
   title = 'chronocrator';
-  hours = moment().format('h')
-  minutes = moment().format('mm')
+  hours = moment().format('h');
+  minutes = moment().format('mm');
   milliseconds = moment().diff(moment().startOf('day'), 'milliseconds');
   seconds = moment().diff(moment().startOf('day'), 'seconds');
   dives = Math.trunc(this.milliseconds / 45000);
-}
+  remainingMetrics = ['fifth', 'sixth', 'seventh', 'eighth'];
+  currentMetrics = ['first'];
 
+  ngAfterViewInit() {
+  }
+
+  addUnit() {
+    const newElement = this.remainingMetrics.pop()!;
+    this.currentMetrics.push(newElement)
+  }
+
+  updateTime() {
+    this.hours = moment().format('h');
+    this.minutes = moment().format('mm'); 
+    this.seconds = moment().diff(moment().startOf('day'), 'seconds')
+  }
+
+}
 
